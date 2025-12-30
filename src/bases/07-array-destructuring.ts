992afddd7f7776bf2f3fdf90667550e355d183c6
Libardo Andrés Cáceres Jaime <libardo.caceres92@gmail.com>
2025-12-30 16:54:46 -0500

    feature: destructuracion de objetos
    destructuracion de objetos

---
+++ src/bases/07-array-destructuring.ts
@@ -0,0 +1,21 @@
+const characterNames = ['Goku', 'Vegeta', 'Trunks'] ;
+
+const [p1, p2, p3] = characterNames;
+
+console.log( {p1} );
+console.log( {p2} );
+console.log( {p3} );
+
+
+const [, , v3] = characterNames;
+
+console.log( {variable3 : v3} );
+
+
+const returnArrayFn = (): [string, number] => {
+    return ['ABC', 123] ;
+}
+
+const [letters, numbers] = returnArrayFn();
+
+console.log( numbers + 100 );
